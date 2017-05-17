18815e376f6d61480df26ef2b4c50da02300b266
Zackangquan <3109098631@qq.com>
2017-05-17 10:38:05 +0800

    第三次eclipse提交++2

--- src/test/git/TestGit.java
+++ src/test/git/TestGit.java
@@ -2,6 +2,6 @@ package test.git;
 
 public class TestGit {
 	public static void main(String[] args) {
-		System.out.println("使用eclipse测试git..++");
+		System.out.println("使用eclipse测试git..++2");
 	}
 }
